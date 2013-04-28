230d750df9ff6c87d2d11d711650303f266638f3
Fabian Bornhofen <fbornhofen@gmail.com>
2013-04-28 13:56:18 +0200

    playing with thresholding, contours

--- src/ocr/furryocr/main.cpp
+++ src/ocr/furryocr/main.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
+#include <vector>
 #include <cv.h>
 #include <highgui.h>
+
+using namespace std;
+using namespace cv;
 
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        std::cerr << "usage: " << argv[0] << " IMAGE" << std::endl;
+        cerr << "usage: " << argv[0] << " IMAGE" << endl;
     }
-    cv::Mat img = cv::imread(argv[1], 0);
-    cv::namedWindow("image");
+    Mat img = imread(argv[1], 0);
+    namedWindow("image");
+    namedWindow("processed");
+    Mat blurred;
+    GaussianBlur(img, blurred, Size(5, 5), 0);
+    Mat thresh;
+    adaptiveThreshold(blurred, thresh, 255, 1, 1, 11, 2);
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(thresh, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    Mat processed = Mat::zeros(thresh.size(), thresh.type());
+    for (int i = 0; i< contours.size(); i++) {
+        Scalar color = Scalar(255, 255, 255);
+        cout << "Contour area: " << contourArea(contours[i]) << endl;
+        drawContours(processed, contours, i, color, 2, 8, hierarchy, 0, Point());
+    }
+
     char c = 0;
     while (c != 27) {
-        cv::imshow("image", img);
-        c = cv::waitKey(0);
+        imshow("image", img);
+        imshow("processed", processed);
+        c = waitKey(0);
     }
     return 0;
 }
